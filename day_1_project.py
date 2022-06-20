09e36e0cfd1d9dba110238a9943f9e0f3aa3f489
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-06-20 08:33:43 +0300

    day 1 project #100days of code

---
+++ day_1_project.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your program.
+print("Welcome to Rockstar Band Name Generator v1.0")
+
+city_grow = input("Which city did you grow up in?\n")
+
+pet_name = input("What is your pets name?\n")
+
+band_name = city_grow +" "+ pet_name
+
+print(band_name)
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
