b9a853fa3c9c7fe278d4cd8549282de82967237e
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-06-20 08:34:44 +0300

    Calculates the length of a name after receiving input from a used

---
+++ Day1.py
@@ -0,0 +1,8 @@
+# print("This is day 1 of 100 days of code\nThe goal is to perfect python programming and switch industries\nTo a higher glory and beyond")
+#
+
+#This code gets input from a user and the counts the characters in the response given by the user and prints the value
+# s = input("What is your name? ")
+# print(len(s))
+
+print(len(input("What is your name? ")))
