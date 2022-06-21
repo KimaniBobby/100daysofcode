a6046bdfaf15effdec940c6b30f7271c27c9f7a2
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-06-21 09:16:10 +0300

    Mathematical operations

---
+++ mathematical_operations.py
@@ -0,0 +1,22 @@
+# Maths Operations
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+# PEMDASLR (Parentheses, Exponents, Multiply & Divide, Add & Subtract, Left to Right)
+# ()
+# **
+# * /
+# + -
+
+#BODMASLR(LEFT TO RIGHT)
+#BRACKETS
+#0PERAND/EXPONENTS
+#DIVISION
+#MULTIPLICATION
+#ADDITION
+#SUBTRACTION
+
+print(3 * (3 + 3) / 3 - 3)
