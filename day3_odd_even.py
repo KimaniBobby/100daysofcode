90a75e4d18442c1f6dd59c43156006873446407f
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-06-23 20:08:40 +0300

    have a number you wanna know if it is odd or even clone this to help you figure it out

---
+++ day3_odd_even.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+new_number = number % 2
+
+if new_number > 0:
+    print("This is an odd number")
+else:
+    print("This is an even number")
+
+
