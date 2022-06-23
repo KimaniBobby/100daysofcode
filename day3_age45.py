344caf3e51c1c380ffbc770ce02873d3039201e2
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-06-23 19:59:24 +0300

    This is app lets elders ride on a rollercoaster ride for free if aged between 45 and 55

---
+++ day3_age45.py
@@ -0,0 +1,34 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster")
+  age = int(input("What is your age?"))
+
+  if age < 12:
+    bill = 5
+    print("Please pay $5")
+  elif age <= 18:
+    bill = 7
+    print("Please pay $7")
+  elif age>=18 and age<45 :
+    bill = 12
+    print("Please pay $12")
+  elif age >=45 and age <= 55:
+        bill = 0
+        print("You have no bill to pay")
+
+    
+  wants_photos = input("Do you want photos Y or N ")
+
+  if wants_photos == "Y":
+    bill +=3
+  
+  print(f"Your final bill is ${bill}")
+else:
+  print("You can't ride the rollercoaster")
+
+
+#   if age >=45 and age<= 55:
+#         print("You have no bill to pay")
