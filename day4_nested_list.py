1438f255d28a6eec56410c27795448bfe5d6e1ed
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-07-06 16:41:10 +0300

    Learn how to create a list with multiple lists in it

---
+++ day4_nested_list.py
@@ -0,0 +1,15 @@
+#nested lists are lists within a list
+
+fruits = ["strawberries","Netarines","Apples","Grapes","Peaches",""]
+vegetables = ["Spinach","Kale","Tomatoes","Celery","Potatoes"] 
+
+dirty_dozen = [fruits,vegetables]
+
+#print(dirty_dozen)
+
+dirty_dozen [1] [1] = ""
+print(dirty_dozen [1] [1])
+
+
+# print(f"{fruits}\n{vegetables}")
+# #print(f"{row1}\n{row2}\n{row3}")
