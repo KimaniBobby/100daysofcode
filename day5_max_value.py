79581a92e826cc70a932ceae2e251c629c55db7f
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-07-13 14:55:00 +0300

    finds the max value in a list without the use of the max function

---
+++ day5_max_value.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+num = 0
+
+for big_num in student_scores:
+    if big_num > num:
+        num = big_num
+print(f"The highest score in the class is: {num}")
