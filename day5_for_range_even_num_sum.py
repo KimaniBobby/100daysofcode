73786c4aa484f7c2374f386da240798186d4a419
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-07-13 14:52:36 +0300

    Gets the sum of even numbers within a range

---
+++ day5_for_range_even_num_sum.py
@@ -0,0 +1,15 @@
+even = 0
+
+for e in range (1,101):
+    if e == 0:
+        even += e
+print(even)
+
+#method 2
+
+# even2 = 0
+
+# for e in range (1,101):
+#     if e % 2 == 0:
+#         even2 += e
+# print(even2)
