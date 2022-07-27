4261e59935f76d7a59b73c206d19d82cec78310e
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-07-27 16:28:40 +0300

    Learning the use of fucntions with inputs

---
+++ day8_functions_inputs.py
@@ -0,0 +1,33 @@
+# def greet():
+#     print("Day 8 start")
+#     print("Road to pro Data scientist")
+#     print("Day 8 gets completed today")
+
+
+# greet()
+
+# name = input("Please enter your name\n")
+
+# def greet_with_name(name):
+#     print(f"Day 8 start {name}")
+#     print(f"Road to pro Data scientist {name}")
+#     print("Day 8 gets completed today")
+
+# greet_with_name(name)
+
+#functions with more than one input
+
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"What is it like {location}")
+
+# greet_with("Bobby","Nairobi")
+
+#functions with keyword arguments
+
+def greet_functions(a,b):
+    print(f"Hello {b}")
+    print(f"What is it like in {a}")
+
+greet_functions(b = "Bobby", a = "Nairobi")
+
