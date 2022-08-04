bcde8a3baf16f29677f07b4c48945145e288f7b5
Bobby Njoroge Kimani <bobbynjorogekimani@Bobbys-MacBook-Pro.local>
2022-08-04 13:10:31 +0300

    Eddited code abit

--- ceasar_cipher_clean.py
+++ ceasar_cipher_clean.py
@@ -3,24 +3,29 @@ from telnetlib import ENCRYPT
 from art import logo
 
 print(logo)
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 
 
 def ceasar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-    
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+
+        else:
+            end_text += char
     print(f"Here's the {direction}d result: {end_text}")
 
 ceasar(start_text = text,  shift_amount = shift, cipher_direction = direction)
